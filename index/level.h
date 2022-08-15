f077edc2f035d467e282fb63b01ed77ac5a0f161
cs1033 <cs1033@mail.ustc.edu.cn>
2022-08-15 08:18:14 +0000

    08-15

--- index/level.h
+++ index/level.h
@@ -150,6 +150,7 @@ namespace level {
                 for (int i = 0; i <= addr_capacity_ / 2; ++i) {
                     clwb(&(buckets_[1][i].state_), 64);                    
                 } 
+                mfence();
                 entrance_->buckets_[0] = galc->relative(buckets_[0]);
                 entrance_->buckets_[1] = galc->relative(buckets_[1]);
                 entrance_->interim_level_buckets_ = nullptr;
@@ -166,7 +167,23 @@ namespace level {
         }
         
         bool Get(_key_t key, _value_t& value) {
-            
+            uint64_t f_hash = hash1(key);
+            uint64_t s_hash = hash2(key);
+            uint64_t f_index = F_IDX(f_hash, addr_capacity_);
+            uint64_t s_index = S_IDX(s_hash, addr_capacity_);
+
+            for (int i = 0; i < 2; ++i) {
+                if (buckets_[i][f_index].Get(key, value)) {
+                    return true;
+                }
+                if (buckets_[i][s_index].Get(key, value)) {
+                    return true;
+                }
+                f_index = F_IDX(f_hash, addr_capacity_ / 2);
+                s_index = S_IDX(s_hash, addr_capacity_ / 2);
+            }
+
+            return false;
         }
 
         bool Insert(_key_t key, _value_t value) {
@@ -200,7 +217,47 @@ namespace level {
         bool Expand() {
             uint64_t new_level = level_ + 1;
             uint64_t new_addr_capacity = (uint64_t)1 << new_level;
+            
+            /* allocate and persist */
             interim_level_buckets_ = (bucket*) galc->malloc(sizeof(bucket) * (new_addr_capacity + 1));
+            entrance_->interim_level_buckets_ = galc->relative(interim_level_buckets_);
+            clwb(entrance_->interim_level_buckets_, sizeof(bucket));
+            mfence();
+
+            /* copy entries */
+            for (int i = 0; i < addr_capacity_ / 2; i++) {
+                for (int j = 0; j < ASSOC_NUM; ++j) {
+                    if (buckets_[1][i].state_.read(j)) {
+                        _key_t  key = buckets_[1][i].slot_[j].key;
+                        _value_t value = (_value_t)buckets_[1][i].slot_[j].val;
+                        uint64_t f_hash = hash1(key);
+                        uint64_t s_hash = hash2(key);
+                        uint64_t f_index = F_IDX(f_hash, new_addr_capacity);
+                        uint64_t s_index = S_IDX(s_hash, new_addr_capacity);
+                        if (!interim_level_buckets_[f_index].Insert(key, value)) {
+                            interim_level_buckets_[s_index].Insert(key, value);
+                        }
+                    }
+                }
+            }
+
+            /* persist */
+            for (int i = 0; i <= new_addr_capacity; ++i) {
+                clwb(&(interim_level_buckets_[i].state_), 64);                    
+            } 
+            mfence();
+
+            /* pointers consistency */
+            buckets_[1] = buckets_[0];
+            buckets_[0] = interim_level_buckets_;
+            entrance_->buckets_[1] = entrance_->buckets_[0];
+            clwb(entrance_->buckets_[1], sizeof(bucket));
+            mfence();
+            entrance_->buckets_[0] = entrance_->interim_level_buckets_;
+            clwb(entrance_->buckets_[0], sizeof(bucket));
+            mfence();
+            entrance_->interim_level_buckets_ = nullptr;
+            clwb(entrance_->interim_level_buckets_, sizeof(bucket));
         }    
 
 
